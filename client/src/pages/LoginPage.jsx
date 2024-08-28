4b17865a7d67539630835970ac11d4e43e1702e0
Branden Orefice <Bran.Orefice@gmail.com>
2024-08-28 18:11:15 -0400

    make it better

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -28,19 +28,17 @@ function LoginPage() {
       ? "https://sound-wave-app-efde8f11e684.herokuapp.com/login"
       : "http://localhost:8888/login";
 
-  function handleLogin() {
-    window.location.href = REACT_APP_LOGIN_URI;
-  }
-
   return (
     <main className={styles.loginpage}>
       <section>
         <h1>Sound Wave</h1>
         <h2>Create playlists and share with your friends</h2>
         {!token ? (
-          <button className={styles.button} onClick={handleLogin}>
-            Log In To Spotify
-          </button>
+          <Link>
+            <button className={styles.button} to={`${REACT_APP_LOGIN_URI}`}>
+              Log In To Spotify
+            </button>
+          </Link>
         ) : (
           <p>Youre already logged in!</p>
         )}
